3bdb69124c21d976d64dbbfcbc3ff521221873da
Madoka <madokka.kz@gmail.com>
2023-02-20 19:46:00 +0600

    second

--- tsis4/json.py
+++
@@ -1,9 +0,0 @@
-import json
-with open(r"C:\Users\Assan\Downloads\sample-data.json") as inf:
-    data = json.load(inf)
-print("Interface Status")
-print("================================================================================")
-print("DN                                                 Description           Speed    MTU" )
-print("-------------------------------------------------- --------------------  ------  ------")
-for i in data["imdata"]:
-    print( i["l1PhysIf"]["attributes"]['dn'],"                             ",i["l1PhysIf"]["attributes"]['speed'],'',i["l1PhysIf"]["attributes"]['mtu']) if len(i["l1PhysIf"]["attributes"]['dn']) == 42 else print( i["l1PhysIf"]["attributes"]['dn'],"                              ",i["l1PhysIf"]["attributes"]['speed'],'',i["l1PhysIf"]["attributes"]['mtu'])
